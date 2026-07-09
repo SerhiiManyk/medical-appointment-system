f499da0cc92876b68a7f3b54ee48a643145a182b
Serhii Manyk <manukserg1988@gmail.com>
2026-07-09 21:03:26 +0200

    create Specialization enum

---
+++ backend/src/main/java/com/serhiimanyk/backend/enums/Specialization.java
@@ -0,0 +1,13 @@
+package com.serhiimanyk.backend.enums;
+
+public enum Specialization {
+
+    FAMILY_DOCTOR,
+    CARDIOLOGIST,
+    DENTIST,
+    SURGEON,
+    DERMATOLOGIST,
+    PEDIATRICIAN,
+    NEUROLOGIST
+
+}
