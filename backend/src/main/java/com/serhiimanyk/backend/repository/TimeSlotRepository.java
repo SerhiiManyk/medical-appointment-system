535125cd17f1846edb800d713a0a2904d4ecb72c
Serhii Manyk <manukserg1988@gmail.com>
2026-07-11 19:18:29 +0200

    create PatientRepository, AppointmentRepository, TimeSlotRepository, DoctorRepository

---
+++ backend/src/main/java/com/serhiimanyk/backend/repository/TimeSlotRepository.java
@@ -0,0 +1,7 @@
+package com.serhiimanyk.backend.repository;
+
+import com.serhiimanyk.backend.entity.TimeSlot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
+}
