1691d4692504348c275549d3f3e72de43821128f
Serhii Manyk <manukserg1988@gmail.com>
2026-07-12 11:57:56 +0200

    refactor DoctorRepository and start create DoctorServiceImpl

--- backend/src/main/java/com/serhiimanyk/backend/repository/DoctorRepository.java
+++ backend/src/main/java/com/serhiimanyk/backend/repository/DoctorRepository.java
@@ -1,7 +1,17 @@
 package com.serhiimanyk.backend.repository;
 
 import com.serhiimanyk.backend.entity.Doctor;
+import com.serhiimanyk.backend.enums.Specialization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+    Optional<Doctor> findByEmail(String email);
+
+    List<Doctor> findBySpecialization(Specialization specialization);
+
+    boolean existsByEmail(String email);
 }
---
+++ backend/src/main/java/com/serhiimanyk/backend/service/impl/DoctorServiceImpl.java
@@ -0,0 +1,51 @@
+package com.serhiimanyk.backend.service.impl;
+
+import com.serhiimanyk.backend.entity.Doctor;
+import com.serhiimanyk.backend.enums.Specialization;
+import com.serhiimanyk.backend.service.DoctorService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoctorServiceImpl implements DoctorService {
+    @Override
+    public Doctor getDoctorByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public Doctor getDoctorById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Doctor> getBySpecialization(Specialization specialization) {
+        return List.of();
+    }
+
+    @Override
+    public void checkEmailUnique(String email) {
+
+    }
+
+    @Override
+    public List<Doctor> getAllDoctors() {
+        return List.of();
+    }
+
+    @Override
+    public Doctor createDoctor(Doctor doctor) {
+        return null;
+    }
+
+    @Override
+    public Doctor updateDoctor(Doctor doctor) {
+        return null;
+    }
+
+    @Override
+    public void deleteDoctorById(Long id) {
+
+    }
+}
