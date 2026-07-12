976a679a0523c70593b706f64c2c5b4c2cd8d95c
Serhii Manyk <manukserg1988@gmail.com>
2026-07-12 12:37:06 +0200

    work with getDoctorByEmail() method in DoctorServiceImpl + create DoctorNotFoundException

--- backend/src/main/java/com/serhiimanyk/backend/service/impl/DoctorServiceImpl.java
+++ backend/src/main/java/com/serhiimanyk/backend/service/impl/DoctorServiceImpl.java
@@ -2,16 +2,32 @@ package com.serhiimanyk.backend.service.impl;
 
 import com.serhiimanyk.backend.entity.Doctor;
 import com.serhiimanyk.backend.enums.Specialization;
+import com.serhiimanyk.backend.exception.DoctorNotFoundException;
+import com.serhiimanyk.backend.repository.DoctorRepository;
 import com.serhiimanyk.backend.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
 
 @Service
 public class DoctorServiceImpl implements DoctorService {
+
+
+    private final DoctorRepository doctorRepository;
+
+    public DoctorServiceImpl(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+
     @Override
     public Doctor getDoctorByEmail(String email) {
-        return null;
+        return doctorRepository.findByEmail(email)
+                .orElseThrow(() ->new DoctorNotFoundException("Doctor with email " + email + " is not found"));
     }
 
     @Override
