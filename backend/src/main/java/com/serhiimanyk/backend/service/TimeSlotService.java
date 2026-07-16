f0732606e1a2db5f1d63bfe4fded4159e6192e97
Serhii Manyk <manukserg1988@gmail.com>
2026-07-16 20:48:44 +0200

    create TimeSlotService

---
+++ backend/src/main/java/com/serhiimanyk/backend/service/TimeSlotService.java
@@ -0,0 +1,24 @@
+package com.serhiimanyk.backend.service;
+
+import com.serhiimanyk.backend.entity.TimeSlot;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TimeSlotService {
+
+    TimeSlot createTimeSlot(TimeSlot timeSlot);
+
+    TimeSlot getTimeSlotById(Long id);
+
+    List<TimeSlot> getAllTimeSlotsByDoctorId(Long doctorId);
+
+    List<TimeSlot> getAvailableTimeSlotsByDoctorId(Long doctorId);
+
+    List<TimeSlot> getAvailableTimeSlotsByDate(LocalDate date);
+
+    void blockTimeSlot(Long timeSlotId);
+
+    void deleteTimeSlot(Long id);
+
+}
