abc39d013a0493a92463e39cf21d6627b9a73be3
Serhii Manyk <manukserg1988@gmail.com>
2026-07-24 19:43:08 +0200

    work with getAllDoctors() method in DoctorController

--- backend/src/main/java/com/serhiimanyk/backend/controller/DoctorController.java
+++ backend/src/main/java/com/serhiimanyk/backend/controller/DoctorController.java
@@ -8,13 +8,11 @@ import com.serhiimanyk.backend.service.DoctorService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/doctors")
@@ -36,5 +34,15 @@ public class DoctorController {
        URI uri =  ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(resultDoctor.getId()).toUri();
 
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DoctorResponse>> getAllDoctors() {
+
+        List<Doctor> doctors = doctorService.getAllDoctors();
+
+        List<DoctorResponse> resultList = doctorMapper.toDoctorsResponseList(doctors);
+
+        return ResponseEntity.ok(resultList);
     }
 }
