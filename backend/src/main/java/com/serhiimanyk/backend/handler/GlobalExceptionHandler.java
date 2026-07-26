74cf4790ea820bebed079903c4046fd59e1f8ba6
Serhii Manyk <manukserg1988@gmail.com>
2026-07-26 15:21:26 +0200

    refactor GlobalExceptionHandler

--- backend/src/main/java/com/serhiimanyk/backend/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/serhiimanyk/backend/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.serhiimanyk.backend.handler;
 
 import com.serhiimanyk.backend.dto.response.ErrorResponse;
 import com.serhiimanyk.backend.exception.DoctorNotFoundException;
+import com.serhiimanyk.backend.exception.EmailAlreadyExistsException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +14,32 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    public ErrorResponse buildErrorResponse(HttpStatus status, String message, HttpServletRequest request) {
+
+        return ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status)
+                .code(status.value())
+                .message(message)
+                .path(request.getRequestURI())
+                .build();
+    }
+
     @ExceptionHandler(DoctorNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleDoctorNotFoundException(DoctorNotFoundException exception,
                                                                        HttpServletRequest request) {
-        ErrorResponse response = ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.NOT_FOUND)
-                .code(HttpStatus.NOT_FOUND.value())
-                .message(exception.getMessage())
-                .path(request.getRequestURI())
-                .build();
+
+        ErrorResponse response = buildErrorResponse(HttpStatus.NOT_FOUND, exception.getMessage(), request);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException exception,
+                                                                           HttpServletRequest request) {
+
+        ErrorResponse response = buildErrorResponse(HttpStatus.CONFLICT, exception.getMessage(), request);
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 }
