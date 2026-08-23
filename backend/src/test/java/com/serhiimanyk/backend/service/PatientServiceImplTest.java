667b0ac0a9394faed7ff5b8db0d4c4d556fed48e
Serhii Manyk <manukserg1988@gmail.com>
2026-08-23 12:29:57 +0200

    work with PatientServiceImplTest(getPatientByEmail(), getPatientById(), getAllPatients() methods)

--- backend/src/test/java/com/serhiimanyk/backend/service/PatientServiceImplTest.java
+++ backend/src/test/java/com/serhiimanyk/backend/service/PatientServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.serhiimanyk.backend.service;
 
 import com.serhiimanyk.backend.entity.Patient;
+import com.serhiimanyk.backend.exception.PatientNotFoundException;
 import com.serhiimanyk.backend.mapper.PatientMapper;
 import com.serhiimanyk.backend.repository.PatientRepository;
 import com.serhiimanyk.backend.service.impl.PatientServiceImpl;
@@ -10,6 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PatientServiceImplTest {
@@ -34,26 +42,79 @@ public class PatientServiceImplTest {
 
     @Test
     public void getPatientByEmail_shouldReturnPatientSuccessfully() {
+
+        when(patientRepository.findByEmail(patient.getEmail())).thenReturn(Optional.of(patient));
+
+        Patient result = patientService.getPatientByEmail(patient.getEmail());
+
+        assertEquals(patient, result);
+
+        verify(patientRepository, times(1)).findByEmail(patient.getEmail());
     }
 
     @Test
     public void getPatientByEmail_shouldThrowExceptionWhenPatientNotFound() {
+
+        when(patientRepository.findByEmail(patient.getEmail())).thenReturn(Optional.empty());
+
+        PatientNotFoundException exception = assertThrows(PatientNotFoundException.class,
+                () -> patientService.getPatientByEmail(patient.getEmail()));
+
+        assertEquals("Patient with email " + patient.getEmail() + " is not found", exception.getMessage());
+
+        verify(patientRepository, times(1)).findByEmail(patient.getEmail());
     }
 
     @Test
     public void getPatientById_shouldReturnPatientSuccessfully() {
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+
+        Patient result = patientService.getPatientById(patient.getId());
+
+        assertEquals(patient, result);
+
+        verify(patientRepository, times(1)).findById(patient.getId());
     }
 
     @Test
     public void getPatientById_shouldThrowExceptionWhenPatientNotFound() {
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.empty());
+
+        PatientNotFoundException exception = assertThrows(PatientNotFoundException.class,
+                () -> patientService.getPatientById(patient.getId()));
+
+        assertEquals("Patient with id " + patient.getId() + " is not found", exception.getMessage());
+
+        verify(patientRepository, times(1)).findById(patient.getId());
     }
 
     @Test
     public void getAllPatients_shouldReturnPatientsSuccessfully() {
+
+        Patient patient1 = new Patient();
+        patient1.setId(2L);
+
+        List<Patient> patientList = List.of(patient1, patient);
+
+        when(patientRepository.findAll()).thenReturn(patientList);
+
+        List<Patient> result = patientService.getAllPatients();
+
+        assertEquals(patientList, result);
+        verify(patientRepository, times(1)).findAll();
     }
 
     @Test
     public void getAllPatients_shouldReturnEmptyListWhenNoPatientsFound() {
+
+        when(patientRepository.findAll()).thenReturn(List.of());
+
+        List<Patient> result = patientService.getAllPatients();
+
+        assertEquals(0, result.size());
+        verify(patientRepository, times(1)).findAll();
     }
 
     @Test
