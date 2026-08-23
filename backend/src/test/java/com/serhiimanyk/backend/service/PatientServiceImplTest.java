f39d5825cc587d5d5b6d5bd42bf950b582bda606
Serhii Manyk <manukserg1988@gmail.com>
2026-08-23 16:16:50 +0200

    work with PatientServiceImplTest(createPatient(), updatePatient(), deletePatientById() methods)

--- backend/src/test/java/com/serhiimanyk/backend/service/PatientServiceImplTest.java
+++ backend/src/test/java/com/serhiimanyk/backend/service/PatientServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.serhiimanyk.backend.service;
 
+import com.serhiimanyk.backend.dto.request.PatientRequest;
 import com.serhiimanyk.backend.entity.Patient;
+import com.serhiimanyk.backend.exception.EmailAlreadyExistsException;
 import com.serhiimanyk.backend.exception.PatientNotFoundException;
 import com.serhiimanyk.backend.mapper.PatientMapper;
 import com.serhiimanyk.backend.repository.PatientRepository;
@@ -119,34 +121,129 @@ public class PatientServiceImplTest {
 
     @Test
     public void createPatient_shouldCreatePatientSuccessfully() {
+
+        when(patientRepository.save(patient)).thenReturn(patient);
+        when(patientRepository.existsByEmail(patient.getEmail())).thenReturn(false);
+
+        Patient result = patientService.createPatient(patient);
+
+        assertEquals(patient, result);
+
+        verify(patientRepository, times(1)).existsByEmail(patient.getEmail());
+        verify(patientRepository, times(1)).save(patient);
     }
 
     @Test
     public void createPatient_shouldThrowExceptionWhenEmailAlreadyExists() {
+
+        when(patientRepository.save(patient)).thenReturn(patient);
+        when(patientRepository.existsByEmail(patient.getEmail())).thenReturn(true);
+
+        EmailAlreadyExistsException exception = assertThrows(EmailAlreadyExistsException.class,
+                () -> patientService.createPatient(patient));
+
+        assertEquals("Patient with email " + patient.getEmail() + " already exists", exception.getMessage());
+        verify(patientRepository, times(1)).existsByEmail(patient.getEmail());
+        verify(patientRepository, never()).save(any());
     }
 
     @Test
     public void updatePatient_shouldUpdatePatientSuccessfullyWhenEmailIsUnchanged() {
+
+        PatientRequest updatePatient = new PatientRequest();
+        updatePatient.setEmail(patient.getEmail());
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+        when(patientRepository.save(patient)).thenReturn(patient);
+
+        Patient result = patientService.updatePatient(updatePatient, patient.getId());
+
+
+        assertEquals(patient, result);
+
+        verify(patientRepository, times(1)).findById(patient.getId());
+        verify(patientRepository, never()).existsByEmailAndIdNot(patient.getEmail(), patient.getId());
+        verify(patientMapper, times(1)).updatePatientFromRequest(updatePatient, patient);
+        verify(patientRepository, times(1)).save(patient);
     }
 
     @Test
     public void updatePatient_shouldUpdatePatientSuccessfullyWhenEmailIsChanged() {
+
+        PatientRequest updatePatient = new PatientRequest();
+        updatePatient.setEmail("change@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+        when(patientRepository.save(patient)).thenReturn(patient);
+        when(patientRepository.existsByEmailAndIdNot(updatePatient.getEmail(), patient.getId())).thenReturn(false);
+
+        Patient result = patientService.updatePatient(updatePatient, patient.getId());
+
+        assertEquals(patient, result);
+        verify(patientRepository, times(1)).findById(patient.getId());
+        verify(patientRepository, times(1)).existsByEmailAndIdNot(updatePatient.getEmail(), patient.getId());
+        verify(patientMapper, times(1)).updatePatientFromRequest(updatePatient, patient);
+        verify(patientRepository, times(1)).save(patient);
     }
 
     @Test
     public void updatePatient_shouldThrowExceptionWhenNewEmailAlreadyExists() {
+
+        PatientRequest updatePatient = new PatientRequest();
+        updatePatient.setEmail("change@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+        when(patientRepository.existsByEmailAndIdNot(updatePatient.getEmail(), patient.getId())).thenReturn(true);
+
+        EmailAlreadyExistsException exception = assertThrows(EmailAlreadyExistsException.class,
+                () -> patientService.updatePatient(updatePatient, patient.getId()));
+
+        assertEquals("Patient with email " + updatePatient.getEmail() + " already exists", exception.getMessage());
+        verify(patientRepository, times(1)).findById(patient.getId());
+        verify(patientRepository,never()).save(any());
+        verify(patientRepository, times(1)).existsByEmailAndIdNot(updatePatient.getEmail(), patient.getId());
     }
 
     @Test
     public void updatePatient_shouldThrowExceptionWhenPatientNotFound() {
+
+        PatientRequest updatePatient = new PatientRequest();
+        updatePatient.setEmail("change@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.empty());
+
+        PatientNotFoundException exception = assertThrows(PatientNotFoundException.class,
+                () ->patientService.updatePatient(updatePatient, patient.getId()));
+
+        assertEquals("Patient with id " + patient.getId() + " is not found", exception.getMessage());
+
+        verify(patientRepository, times(1)).findById(patient.getId());
+        verify(patientRepository,never()).save(any());
     }
 
     @Test
     public void deletePatientById_shouldDeletePatientSuccessfully() {
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+
+        patientService.deletePatientById(patient.getId());
+
+        verify(patientRepository, times(1)).delete(patient);
+        verify(patientRepository, times(1)).findById(patient.getId());
     }
 
     @Test
     public void deletePatientById_shouldThrowExceptionWhenPatientNotFound() {
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.empty());
+
+        PatientNotFoundException exception = assertThrows(PatientNotFoundException.class,
+                () -> patientService.deletePatientById(patient.getId()));
+
+        assertEquals("Patient with id " + patient.getId() + " is not found", exception.getMessage());
+
+        verify(patientRepository, times(1)).findById(patient.getId());
+        verify(patientRepository,never()).delete(any());
     }
 
 }
