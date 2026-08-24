6d216379328a2c4b7b5da4289dcc103c923955f9
Serhii Manyk <manukserg1988@gmail.com>
2026-08-24 20:42:39 +0200

    work with DoctorControllerTest class(createDoctor() method)

--- backend/src/test/java/com/serhiimanyk/backend/controller/DoctorControllerTest.java
+++ backend/src/test/java/com/serhiimanyk/backend/controller/DoctorControllerTest.java
@@ -1,6 +1,7 @@
 package com.serhiimanyk.backend.controller;
 
 import com.serhiimanyk.backend.dto.request.DoctorRequest;
+import com.serhiimanyk.backend.dto.response.DoctorResponse;
 import com.serhiimanyk.backend.entity.Doctor;
 import com.serhiimanyk.backend.enums.Specialization;
 import com.serhiimanyk.backend.mapper.DoctorMapper;
@@ -8,11 +9,17 @@ import com.serhiimanyk.backend.service.DoctorService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.MediaType;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 public class DoctorControllerTest {
@@ -39,29 +46,68 @@ public class DoctorControllerTest {
     }
 
     @Test
-    public void createDoctor_shouldCreateDoctorSuccessfully(){
+    public void createDoctor_shouldCreateDoctorSuccessfully() throws Exception {
 
-        DoctorRequest  doctorRequest = new DoctorRequest();
+        DoctorRequest doctorRequest = new DoctorRequest();
         doctorRequest.setFirstName("firstName");
         doctorRequest.setLastName("lastName");
         doctorRequest.setPhoneNumber("1234567890");
         doctorRequest.setEmail("doctor@test.com");
         doctorRequest.setPassword("password");
         doctorRequest.setSpecialization(Specialization.DENTIST);
+
+        DoctorResponse doctorResponse = new DoctorResponse(
+                doctor.getId(),
+                doctorRequest.getFirstName(),
+                doctorRequest.getLastName(),
+                doctorRequest.getSpecialization());
+
+        when(doctorMapper.toDoctor(any(DoctorRequest.class))).thenReturn(doctor);
+        when(doctorMapper.toDoctorResponse(doctor)).thenReturn(doctorResponse);
+        when(doctorService.createDoctor(doctor)).thenReturn(doctor);
+
+        mockMvc.perform(
+                        post("/api/doctors")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                          "firstName": "firstName",
+                                          "lastName": "lastName",
+                                          "phoneNumber": "1234567890",
+                                          "email": "doctor@test.com",
+                                          "password": "password",
+                                          "specialization": "DENTIST"
+                                        }
+                                        """))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "http://localhost/api/doctors/1"))
+                .andExpect(jsonPath("$.firstName").value("firstName"))
+                .andExpect(jsonPath("$.lastName").value("lastName"))
+                .andExpect(jsonPath("$.specialization").value("DENTIST"))
+                .andExpect(jsonPath("$.id").value(1));
+
+        verify(doctorMapper, times(1)).toDoctor(any(DoctorRequest.class));
+        verify(doctorService, times(1)).createDoctor(doctor);
+        verify(doctorMapper, times(1)).toDoctorResponse(doctor);
     }
 
     @Test
-    public void getAllDoctors_shouldReturnDoctorsSuccessfully(){}
+    public void getAllDoctors_shouldReturnDoctorsSuccessfully() {
+    }
 
     @Test
-    public void getAllDoctors_shouldReturnEmptyListWhenNoDoctorsFound(){}
+    public void getAllDoctors_shouldReturnEmptyListWhenNoDoctorsFound() {
+    }
 
     @Test
-    public void getDoctorById_shouldReturnDoctorSuccessfully(){}
+    public void getDoctorById_shouldReturnDoctorSuccessfully() {
+    }
 
     @Test
-    public void updateDoctor_shouldUpdateDoctorSuccessfully(){}
+    public void updateDoctor_shouldUpdateDoctorSuccessfully() {
+    }
 
     @Test
-    public void deleteDoctorById_shouldDeleteDoctorSuccessfully(){}
+    public void deleteDoctorById_shouldDeleteDoctorSuccessfully() {
+    }
 }
