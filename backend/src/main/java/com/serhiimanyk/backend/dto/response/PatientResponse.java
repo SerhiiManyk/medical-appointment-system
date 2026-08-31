d70e783e7aa295fd3a07357bda01a065713182e1
Serhii Manyk <manukserg1988@gmail.com>
2026-08-31 19:25:24 +0200

    work with PatientControllerTest (createPatient() method)

--- backend/src/main/java/com/serhiimanyk/backend/dto/response/PatientResponse.java
+++ backend/src/main/java/com/serhiimanyk/backend/dto/response/PatientResponse.java
@@ -1,5 +1,6 @@
 package com.serhiimanyk.backend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.serhiimanyk.backend.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,5 +21,6 @@ public class PatientResponse {
 
     private Gender gender;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 }
--- backend/src/test/java/com/serhiimanyk/backend/controller/PatientControllerTest.java
+++ backend/src/test/java/com/serhiimanyk/backend/controller/PatientControllerTest.java
@@ -1,16 +1,29 @@
 package com.serhiimanyk.backend.controller;
 
+import com.serhiimanyk.backend.dto.request.PatientRequest;
+import com.serhiimanyk.backend.dto.response.PatientResponse;
 import com.serhiimanyk.backend.entity.Patient;
+import com.serhiimanyk.backend.enums.Gender;
 import com.serhiimanyk.backend.handler.GlobalExceptionHandler;
 import com.serhiimanyk.backend.mapper.PatientMapper;
 import com.serhiimanyk.backend.service.PatientService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(MockitoExtension.class)
 public class PatientControllerTest {
@@ -39,5 +52,49 @@ public class PatientControllerTest {
         patient.setFirstName("John");
         patient.setLastName("Doe");
         patient.setEmail("patient@mail.com");
+        patient.setGender(Gender.MALE);
+        patient.setDateOfBirth(LocalDate.of(1989, 7, 7));
+    }
+
+    @Test
+    public void createPatient_shouldCreatePatientSuccessfully() throws Exception {
+
+        PatientResponse patientResponse = new PatientResponse(
+                patient.getId(),
+                patient.getFirstName(),
+                patient.getLastName(),
+                patient.getGender(),
+                patient.getDateOfBirth()
+        );
+
+        when(patientMapper.toPatient(any(PatientRequest.class))).thenReturn(patient);
+        when(patientMapper.toPatientResponse(patient)).thenReturn(patientResponse);
+        when(patientService.createPatient(patient)).thenReturn(patient);
+
+        mockMvc.perform(
+                post("/api/patients")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                 {
+                                   "firstName": "John",
+                                   "lastName": "Doe",
+                                   "phoneNumber": "1234567890",
+                                   "email": "patient@mail.com",
+                                   "password": "password",
+                                   "gender": "MALE",
+                                   "dateOfBirth": "1989-07-07"
+                                 }
+                                """))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "http://localhost/api/patients/1"))
+                .andExpect(jsonPath("$.firstName").value("John"))
+                .andExpect(jsonPath("$.lastName").value("Doe"))
+                .andExpect(jsonPath("$.gender").value("MALE"))
+                .andExpect(jsonPath("$.dateOfBirth").value("1989-07-07"))
+                .andExpect(jsonPath("$.id").value(1));
+
+        verify(patientMapper,times(1)).toPatient(any(PatientRequest.class));
+        verify(patientMapper,times(1)).toPatientResponse(patient);
+        verify(patientService,times(1)).createPatient(patient);
     }
 }
