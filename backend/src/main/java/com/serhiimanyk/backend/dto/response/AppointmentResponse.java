873548758a59895d3f658dc20134fdc1d374cc99
Serhii Manyk <manukserg1988@gmail.com>
2026-09-10 20:40:08 +0200

    work with AppointmentControllerTest(cancelAppointmentById() method )

--- backend/src/main/java/com/serhiimanyk/backend/dto/response/AppointmentResponse.java
+++ backend/src/main/java/com/serhiimanyk/backend/dto/response/AppointmentResponse.java
@@ -6,11 +6,13 @@ import com.serhiimanyk.backend.enums.Specialization;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 public class AppointmentResponse {
--- backend/src/test/java/com/serhiimanyk/backend/controller/AppointmentControllerTest.java
+++ backend/src/test/java/com/serhiimanyk/backend/controller/AppointmentControllerTest.java
@@ -8,6 +8,8 @@ import com.serhiimanyk.backend.entity.Patient;
 import com.serhiimanyk.backend.entity.TimeSlot;
 import com.serhiimanyk.backend.enums.AppointmentStatus;
 import com.serhiimanyk.backend.enums.Specialization;
+import com.serhiimanyk.backend.enums.TimeSlotStatus;
+import com.serhiimanyk.backend.exception.AppointmentAlreadyFinishedException;
 import com.serhiimanyk.backend.exception.AppointmentNotFoundException;
 import com.serhiimanyk.backend.handler.GlobalExceptionHandler;
 import com.serhiimanyk.backend.mapper.AppointmentMapper;
@@ -404,4 +406,74 @@ public class AppointmentControllerTest {
         verify(appointmentService, times(1)).deleteAppointmentById(appointment.getId());
         verifyNoInteractions(appointmentMapper);
     }
+
+    @Test
+    public void cancelAppointmentById_shouldCancelAppointmentSuccessfully() throws Exception {
+
+        appointment.setStatus(AppointmentStatus.CANCELLED);
+        appointment.getTimeSlot().setStatus(TimeSlotStatus.FREE);
+        appointmentResponse.setStatus(AppointmentStatus.CANCELLED);
+
+        when(appointmentService.cancelAppointment(appointment.getId())).thenReturn(appointment);
+        when(appointmentMapper.toResponse(appointment)).thenReturn(appointmentResponse);
+
+        mockMvc.perform(
+                patch("/api/appointments/1/cancel")
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.patientId").value(1L))
+                .andExpect(jsonPath("$.patientFirstName").value("John"))
+                .andExpect(jsonPath("$.patientLastName").value("Doe"))
+                .andExpect(jsonPath("$.doctorId").value(1L))
+                .andExpect(jsonPath("$.doctorFirstName").value("Doctor"))
+                .andExpect(jsonPath("$.doctorLastName").value("Watson"))
+                .andExpect(jsonPath("$.specialization").value("FAMILY_DOCTOR"))
+                .andExpect(jsonPath("$.timeSlotId").value(1L))
+                .andExpect(jsonPath("$.date").value("2030-01-01"))
+                .andExpect(jsonPath("$.startTime").value("08:00"))
+                .andExpect(jsonPath("$.endTime").value("09:00"))
+                .andExpect(jsonPath("$.status").value("CANCELLED"))
+                .andExpect(jsonPath("$.comment").value("Test comment"));
+
+        verify(appointmentService, times(1)).cancelAppointment(appointment.getId());
+        verify(appointmentMapper, times(1)).toResponse(appointment);
+    }
+
+    @Test
+    public void cancelAppointmentById_shouldReturn404WhenAppointmentNotFound()  throws Exception {
+
+        when(appointmentService.cancelAppointment(appointment.getId())).thenThrow(new AppointmentNotFoundException("Appointment not found"));
+
+        mockMvc.perform(
+                patch("/api/appointments/1/cancel")
+        )
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Appointment not found"));
+
+        verify(appointmentService, times(1)).cancelAppointment(appointment.getId());
+        verifyNoInteractions(appointmentMapper);
+    }
+
+    @Test
+    public void cancelAppointmentById_shouldReturn400WhenAppointmentIsAlreadyCancelled()  throws Exception {
+
+        appointment.setStatus(AppointmentStatus.CANCELLED);
+        appointmentResponse.setStatus(AppointmentStatus.CANCELLED);
+
+        when(appointmentService.cancelAppointment(appointment.getId())).thenThrow(
+                new AppointmentAlreadyFinishedException("Appointment is already cancelled or completed."));
+
+        mockMvc.perform(
+                        patch("/api/appointments/1/cancel")
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Appointment is already cancelled or completed."));
+
+        verify(appointmentService, times(1)).cancelAppointment(appointment.getId());
+        verifyNoInteractions(appointmentMapper);
+    }
+
+    @Test
+    public void completeAppointmentById_shouldCompleteAppointmentSuccessfully() throws Exception {}
 }
